cfd5e6c4151a061bc5175ecbaa606609a19a72bd
Julio Cesar <julioreis044@gmail.com>
2024-02-18 13:18:10 -0300

    Treinando minha logica

---
+++ MeuPromeiroProjetoConsole/MeuPromeiroProjetoConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPromeiroProjetoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ola mundo, me livrei da maldicao");
+            Console.ReadKey();
+        }
+    }
+}
